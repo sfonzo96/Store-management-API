c6ae8045a597aa89de00d2831b7d5ad985900fa1
santif <santiagofonzo@live.com>
2023-07-17 16:04:03 -0300

    [FIX] URL FIX

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -63,7 +63,7 @@ const passportConfig = async (passport) => {
         clientID: config.GH_CLIENT_ID,
         clientSecret: config.GH_CLIENT_SECRET,
         callbackURL:
-          'https://coderhouse-backend-final.onrender.com/api/v1/docs/api/github/callback',
+          'https://coderhouse-backend-final.onrender.com/api/github/callback',
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
